daf91443038ae277abf3a310e7ee0e2717fe2545
eugenepla <svetlanovich93@gmail.com>
2019-05-20 14:32:22 +0300

    fix timestamp loading speed

--- PlayerPage/Details.js
+++ PlayerPage/Details.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 
-const BUFFERING_STRING = '00:00 / 00:00'
-
 export default function Details(props) {
   return (
     <View style={styles.detailsContainer}>
-      <Text style={styles.timer}>
-        {props.isBuffering ? BUFFERING_STRING : props.getTimestamp}
-      </Text>
+      <Text style={styles.timer}>{props.getTimestamp}</Text>
     </View>
   )
 }
